054da240516619c6577ca90034ce8cef8ddebeb7
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2025-09-05 22:28:39 -0300

    Creando mi Primer Componente

---
+++ MiPrimerProyectoReact/src/PorfolioApp.jsx
@@ -0,0 +1,11 @@
+import './PorfolioApp.css'
+
+export const PorfolioApp = () => {
+  return (
+    <div>
+      <h1 className='rojo'>Esta es mi aplicación de Porfolio</h1>
+      <h4>Este es mi primer componente!</h4>
+      <p>Hola Mundo desde React!</p>
+    </div>
+  );
+};
---
+++ MiPrimerProyectoReact/src/main.jsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { PorfolioApp } from './PorfolioApp'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PorfolioApp />
+  </React.StrictMode> 
+)
