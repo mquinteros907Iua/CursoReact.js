9939fbc46e89fd5d8ee624e45d4208723d2ceefc
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2025-09-06 09:15:43 -0300

    Estilos parte 2

--- MiPrimerProyectoReact/src/VariablesApp.jsx
+++ MiPrimerProyectoReact/src/VariablesApp.jsx
@@ -1,31 +1,19 @@
 import "./VariablesApp.css";
 
 export const VariablesApp = () => {
+  const active = true;
 
-    const style ={
-        color: 'red',
-        fontSize: '20px'
-    }
+  const style = {
+    color: active ? "red" : "blue",
+    fontSize: "20px"
+  }
 
   return (
     <>
-      <h1>Esta clase es de estilos</h1>
-      <h2 id="subtitulo">Vamos a utilizar distintos selectores</h2>
-      <h4 className="red">Este es un titulo rojo</h4>
-      <div>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat et
-          minus inventore in, reiciendis dicta delectus! Voluptatum maiores
-          magni minus perspiciatis? Debitis, est voluptatem. Vitae quo eligendi
-          id laudantium minima.
-        </p>
-      </div>
-      <p style={style}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sunt
-        ad quo cumque doloribus aliquid quos dolores inventore ratione fugit
-        deserunt impedit, reiciendis omnis distinctio fuga at aut similique!
-        Non.
-      </p>
+      <h1 className={active ? 'active' : 'inactive'}>
+        Esta clase es de estilos
+      </h1>
+      <h1 style={style}></h1>
     </>
   );
 };
